f658928b2f985becdede95bb782aa22a00397f7d
Bhaven Kakade <thisisbhaven@gmail.com>
2022-05-28 11:23:57 +0530

    collaborator in index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,4 @@ module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
 module.exports.bountyController = require('./bounty.controller');
 module.exports.collabController = require('./collab.controller');
+module.exports.collaboratorController = require('./collaborator.controller');
